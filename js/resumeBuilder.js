a67df73344d57c7c633b0dbdbedc5470be7b1f80
Tasoril <me@tasoril.com>
2015-11-20 00:12:04 -0600

    Added extra complexity per reviewer's request.
    Added extra complexity per reviewer's request.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -6,6 +6,7 @@ bio = {
 	"contacts" : {
 		"mobile" : "563-289-7897",
 		"email" : "JohnathonWStevens@gmail.com",
+		"twitter" : "I don't use Twitter, but the reviewer said they wanted this so here it is.",
 		"github" : "Tasoril",
 		"linkedin" : "https://linkedin.com/in/johnathonstevens",
 		"location" : "Dubuque, Iowa"
@@ -22,6 +23,7 @@ bio.display = function() {
 	$("#header").prepend(HTMLheaderName.replace("%data%",bio.name));
 	$("#topContacts").append(HTMLmobile.replace("%data%",bio.contacts.mobile));
 	$("#topContacts").append(HTMLemail.replace("%data%",bio.contacts.email));
+	//$("#topContacts").append(HTMLtwitter.replace("%data%",bio.contacts.twitter));
 	$("#topContacts").append(HTMLgithub.replace("%data%",bio.contacts.github));
 	$("#topContacts").append(HTMLcontactGeneric.replace("%data%",bio.contacts.linkedin).replace("%contact%","linkedin"));
 	$("#topContacts").append(HTMLlocation.replace("%data%",bio.contacts.location));
@@ -41,50 +43,52 @@ bio.display = function() {
 
 
 
-work = [
-	{
-		"employer" : "IBM Corporation",
-		"title" : "Automation Specialist",
-		"location" : "Dubuque, IA, US",
-		"dates" : "11/2013 - 09/2014",
-		"description" : [
-			"Primary duties in this role included updating and maintaining scripts which are used by a team of IT specialists to support a Windows Server environment of 12,000+ Windows 2000-2008 R2 servers.",
-			"Ability and aptitude to efficiently write a web based script to health check servers which saved the team over 120 hours weekly of manual work, in addition to a logging script for government compliance that saved 40 hours of data sifting, prevented data loss, and minimized audit issues.",
-			"Developed and implemented a vacation planner to replace an older outdated manual method, which saved management hundreds of hours of manual work per month.",
-			"Responsible for updating and adding features to existing automation tools written in VBScript, HTML, JavaScript, PHP, and MySQL."
-		]
-	},
-	{
-		"employer" : "IBM Corporation",
-		"title" : "Technical Support Speicalist - Jazz (Tier 3)",
-		"location" : "Dubuque, IA, US",
-		"dates": "09/2011 - 10/2013",
-		"description" : [
-			"Primary duties in this role consisted of providing support for 12,000+ Windows based servers including hardware and OS troubleshooting functions, as well as 2,000+ VMware servers.",
-			"Provided programming and automation support in order to ease status and health checking of servers for speedy resolution of critical outages.", "Tasked with responding to support requests for both major and minor outages of Windows servers or VMs.", "Responsible for the updating and maintaining scripts and programs used to automate common tasks executed by the team.", "Role as management contact for the second shift of employees accountable for Windows based and VMware server support.",
-			"Received highest possible yearly review ratings for each year in this position.",
-			"Successfully saved the team over 1,000 hours per year of manual work through automation.",
-			"Pushed and scheduled patches using BigFix/TEM and SCCM. Monitoring and error reporting using SCCM."
-		]
-	},
-	{
-		"employer" : "IBM Corporation",
-		"title" : "Technical Support Speicalist - Dispatcher",
-		"location" : "Dubuque, IA, US",
-		"dates": "03/2011 - 09/2011",
-		"description" : [
-			"Primary duties in this role consisted of efficiently dispatching tickets for an enterprise client of 12,000+ Windows based servers and virtual machines.",
-			"Direct communication with customers to identify and troubleshoot specific issues, then determine the appropriate course to correct the issue.",
-			"Ability to multitask while completing and dispatching tickets to a dedicated team of Systems Administrators.",
-			"Responsible for initial triage work and reassignment of incorrectly assigned tickets to be expedited."
-		]
-	}
-];
+work = {
+	"jobs" : [
+		{
+			"employer" : "IBM Corporation",
+			"title" : "Automation Specialist",
+			"location" : "Dubuque, IA, US",
+			"dates" : "11/2013 - 09/2014",
+			"description" : [
+				"Primary duties in this role included updating and maintaining scripts which are used by a team of IT specialists to support a Windows Server environment of 12,000+ Windows 2000-2008 R2 servers.",
+				"Ability and aptitude to efficiently write a web based script to health check servers which saved the team over 120 hours weekly of manual work, in addition to a logging script for government compliance that saved 40 hours of data sifting, prevented data loss, and minimized audit issues.",
+				"Developed and implemented a vacation planner to replace an older outdated manual method, which saved management hundreds of hours of manual work per month.",
+				"Responsible for updating and adding features to existing automation tools written in VBScript, HTML, JavaScript, PHP, and MySQL."
+			]
+		},
+		{
+			"employer" : "IBM Corporation",
+			"title" : "Technical Support Speicalist - Jazz (Tier 3)",
+			"location" : "Dubuque, IA, US",
+			"dates": "09/2011 - 10/2013",
+			"description" : [
+				"Primary duties in this role consisted of providing support for 12,000+ Windows based servers including hardware and OS troubleshooting functions, as well as 2,000+ VMware servers.",
+				"Provided programming and automation support in order to ease status and health checking of servers for speedy resolution of critical outages.", "Tasked with responding to support requests for both major and minor outages of Windows servers or VMs.", "Responsible for the updating and maintaining scripts and programs used to automate common tasks executed by the team.", "Role as management contact for the second shift of employees accountable for Windows based and VMware server support.",
+				"Received highest possible yearly review ratings for each year in this position.",
+				"Successfully saved the team over 1,000 hours per year of manual work through automation.",
+				"Pushed and scheduled patches using BigFix/TEM and SCCM. Monitoring and error reporting using SCCM."
+			]
+		},
+		{
+			"employer" : "IBM Corporation",
+			"title" : "Technical Support Speicalist - Dispatcher",
+			"location" : "Dubuque, IA, US",
+			"dates": "03/2011 - 09/2011",
+			"description" : [
+				"Primary duties in this role consisted of efficiently dispatching tickets for an enterprise client of 12,000+ Windows based servers and virtual machines.",
+				"Direct communication with customers to identify and troubleshoot specific issues, then determine the appropriate course to correct the issue.",
+				"Ability to multitask while completing and dispatching tickets to a dedicated team of Systems Administrators.",
+				"Responsible for initial triage work and reassignment of incorrectly assigned tickets to be expedited."
+			]
+		}
+	]
+};
 work.display = function() {
 	// Write Work Information
-	for(var job = 0; job < work.length; job++) {
+	for(var job = 0; job < work.jobs.length; job++) {
 		$("#workExperience").append(HTMLworkStart);
-		var thisJob = work[job];
+		var thisJob = work.jobs[job];
 		var formattedEmployer = HTMLworkEmployer.replace("%data%", thisJob.employer);
 		var formattedTitle = HTMLworkTitle.replace("%data%", thisJob.title);
 		var formattedDates = HTMLworkDates.replace("%data%", thisJob.dates);
@@ -102,19 +106,21 @@ work.display = function() {
 
 
 
-projects = [
-	{
-		"title" : "Jet: Price After Discount",
-		"dates" : "2015",
-		"description" : ["A TamperMonkey script written to show the user the final price for a product without having to calculate it in their head."],
-		"images" : ["images/jetpad-image.png"]
-	}
-];
+projects = {
+	"projects" : [
+		{
+			"title" : "Jet: Price After Discount",
+			"dates" : "2015",
+			"description" : ["A TamperMonkey script written to show the user the final price for a product without having to calculate it in their head."],
+			"images" : ["images/jetpad-image.png"]
+		}
+	]
+};
 projects.display = function() {
 	// Write Project Information
-	for(var project = 0; project < projects.length; project++) {
+	for(var project = 0; project < projects.projects.length; project++) {
 		$("#projects").append(HTMLprojectStart);
-		var thisProject = projects[project];
+		var thisProject = projects.projects[project];
 		var formattedTitle = HTMLprojectTitle.replace("%data%", thisProject.title);
 		var formattedDates = HTMLprojectDates.replace("%data%", thisProject.dates);
 		// Description
